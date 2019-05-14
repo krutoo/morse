fcbd2d50b40b057d66f8337cbcab12c2d32a4653
Petrov Dmitry <dima.krutoo@yandex.ru>
2019-05-14 21:45:48 +0500

    indentation fixed

--- src/transport/observable-queue.js
+++ src/transport/observable-queue.js
@@ -1,26 +1,26 @@
 import { isFunction } from '../utils.js';
 
 export const makeFunctionOnlyPush = list => handler => {
-    isFunction(handler) && list.push(handler);
+  isFunction(handler) && list.push(handler);
 };
 
 export const createObservableQueue = queue => {
-    const handlers = [];
-    const temporaryHandlers = [];
-    return {
-        ...queue,
-        enqueue: item => {
-            queue.enqueue(item);
+  const handlers = [];
+  const temporaryHandlers = [];
+  return {
+    ...queue,
+    enqueue: item => {
+      queue.enqueue(item);
 
-            // при большом количестве слушателей здесь заметно провиснет производительность
-            for (const handler of handlers) {
-                handler(item);
-            }
-            while (temporaryHandlers.length > 0) {
-                temporaryHandlers.pop()(item);
-            }
-        },
-        handleEnqueue: makeFunctionOnlyPush(handlers),
-        handleEnqueueOnce: makeFunctionOnlyPush(temporaryHandlers),
-    };
+      // при большом количестве слушателей здесь заметно провиснет производительность
+      for (const handler of handlers) {
+        handler(item);
+      }
+      while (temporaryHandlers.length > 0) {
+        temporaryHandlers.pop()(item);
+      }
+    },
+    handleEnqueue: makeFunctionOnlyPush(handlers),
+    handleEnqueueOnce: makeFunctionOnlyPush(temporaryHandlers),
+  };
 };
