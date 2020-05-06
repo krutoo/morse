7fd4b6d8b80b33ccf9da41082813adc59bdb88b6
Petrov Dmitry <dima.krutoo@yandex.ru>
2020-05-06 14:31:46 +0500

    isString added

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 export const isFunction = value => typeof value === 'function';
 
+export const isString = value => typeof value === 'string';
+
 export const generateId = () => Math.random().toString(16).slice(2);
 
 export const Validator = (checker, makeErrorText) => value => {
