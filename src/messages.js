6b4b198f14aa37d33eea630290d442d73127c98e
Petrov Dmitry <dima.krutoo@yandex.ru>
2020-05-06 14:32:10 +0500

    message topic validation added

--- src/messages.js
+++ src/messages.js
@@ -1,11 +1,10 @@
 import { Type } from './helpers/type';
-
-export const { is: isMessage, apply: asMessage } = Type('morse::message');
-
-export const { is: isFactory, apply: asFactory } = Type('morse::message-factory');
+import { Validator, isString } from './utils';
 
 const MessageFactory = type => {
   const createFactory = topic => {
+    Validate.topic(topic);
+
     const factory = payload => asMessage({ type, topic, payload });
 
     factory.toString = () => topic;
@@ -20,13 +19,24 @@ const MessageFactory = type => {
   return createFactory;
 };
 
+export const { is: isMessage, apply: asMessage } = Type('morse::message');
+
+export const { is: isFactory, apply: asFactory } = Type('morse::message-factory');
+
+const Validate = {
+  topic: Validator(
+    value => isString(value) && value.length > 0,
+    () => 'Message topic must be a non empty string'
+  ),
+};
+
 const Command = MessageFactory('@@command');
+
+const Response = MessageFactory('@@response'); // eslint-disable-line no-shadow
 
 const Query = MessageFactory('@@query');
 
 Query.responseOf = (message, payload) => Response(`[@@response]${message.topic}`)(payload);
-
-const Response = MessageFactory('@@response'); // eslint-disable-line no-shadow
 
 export {
   Command,
