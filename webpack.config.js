9b876e0fd5b4a7396690870397f65556425503d6
Petrov Dmitry <dima.krutoo@yandex.ru>
2020-05-08 17:58:28 +0500

    lib build config adde

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'morse.js',
+    library: 'Morse',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
