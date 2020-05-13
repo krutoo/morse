609a71fa6402470be631037efaf7809b187ec577
Petrov Dmitry <dima.krutoo@yandex.ru>
2020-05-13 14:45:38 +0500

    isObject added

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,8 @@ export const isFunction = value => typeof value === 'function';
 
 export const isString = value => typeof value === 'string';
 
+export const isObject = value => Object(value) === value;
+
 export const generateId = () => Math.random().toString(16).slice(2);
 
 export const Validator = (checker, makeErrorText) => value => {
