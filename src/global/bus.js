0f4787604029bf7bce83f45b840dff6989cba3fd
Petrov Dmitry <dima.krutoo@yandex.ru>
2020-05-18 22:42:40 +0500

    global bus cqrs middleware added

--- src/global/bus.js
+++ src/global/bus.js
@@ -1,8 +1,9 @@
 import { MessageQueue } from '../helpers/queue';
 import { GLOBAL_BUS_KEY } from '../constants';
+import { globalQueueMiddleware } from '../cqrs';
 
 window[GLOBAL_BUS_KEY] = window[GLOBAL_BUS_KEY] || {
-  globalQueue: MessageQueue(),
+  globalQueue: MessageQueue({ middleware: globalQueueMiddleware }),
 };
 
 export const GlobalBus = window[GLOBAL_BUS_KEY];
