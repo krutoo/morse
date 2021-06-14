deddc0c10913c09e8615f309f21f3dc7cdc48b91
krutoo <dima.krutoo@yandex.ru>
2021-06-14 11:56:10 +0500

    - code style fixes - generateId now takes more unique values

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,19 @@
 import { MessageContainer, Queue, TopicLike } from './types';
 
-export const generateId = () => Math.random().toString(16).slice(2);
+export const generateId = () => Math.random().toString(16).slice(2) + `::${Date.now()}`;
 
 export const mapTopic = (t: TopicLike) => typeof t === 'string' ? t : t.topic;
 
 export const copyMessages = <T extends string> (
   source: Queue<MessageContainer>,
   target: Queue<MessageContainer<T>>,
-  isSuitable: (container: MessageContainer) => container is MessageContainer<T>
+  predicate: (container: MessageContainer) => container is MessageContainer<T>
 ) => {
   const startPosition = source.getSize();
 
   for (let i = 0; i < startPosition; i++) {
     const container = source.getItem(i);
 
-    container && isSuitable(container) && target.enqueue(container);
+    container && predicate(container) && target.enqueue(container);
   }
 };
